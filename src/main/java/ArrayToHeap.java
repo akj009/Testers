8f67df0459445286fba4812155386a642b5d6c6c
akj009 <develop.abhij03@gmail.com>
2018-12-14 17:57:23 +0530

    one-sided-tree for max-heap

--- src/main/java/ArrayToHeap.java
+++ src/main/java/ArrayToHeap.java
@@ -2,10 +2,41 @@ public class ArrayToHeap {
 
   public static void main(String[] args) {
     int inputArr[] = new int[] {2, 5, 8, 30, 20, 7, 9, 6, 25};
-    TreeNode rootNode = new TreeNode(inputArr[0], null, null, null);
-    for (int i = 0; i < inputArr.length; i++) {
-
+    TreeNode rootNode = new TreeNode(inputArr[0]);
+    TreeNode parentNode = rootNode;
+    TreeNode currentNode = null;
+    for (int i = 1; i < inputArr.length; i++) {
+      currentNode = new TreeNode(inputArr[i]);
+      heapify(currentNode, parentNode);
+      bubbleUp(currentNode);
     }
+    System.out.println("done");
+  }
+
+  private static void heapify(TreeNode currentNode, TreeNode parentNode) {
+    if (parentNode.leftChild == null) {
+      parentNode.leftChild = currentNode;
+    } else if (parentNode.rightChild == null) {
+        parentNode.rightChild = currentNode;
+    } else {
+        parentNode = parentNode.leftChild;
+        heapify(currentNode, parentNode);
+        return;
+    }
+    currentNode.parent = parentNode;
+  }
+
+  private static void bubbleUp(TreeNode lastNode) {
+    if (null != lastNode.parent && lastNode.parent.currentValue < lastNode.currentValue) {
+      swapValues(lastNode);
+      bubbleUp(lastNode.parent);
+    }
+  }
+
+  private static void swapValues(TreeNode lastNode) {
+    int tmp = lastNode.parent.currentValue;
+    lastNode.parent.currentValue = lastNode.currentValue;
+    lastNode.currentValue = tmp;
   }
 
   static class TreeNode {
@@ -14,11 +45,8 @@ public class ArrayToHeap {
     TreeNode rightChild;
     TreeNode parent;
 
-    TreeNode(int currentValue, TreeNode leftChild, TreeNode rightChild, TreeNode parent) {
+    TreeNode(int currentValue) {
       this.currentValue = currentValue;
-      this.leftChild = leftChild;
-      this.rightChild = rightChild;
-      this.parent = parent;
     }
   }
 }
